6b39aa3624c957b03cf9778f1cbbcda7e0a87288
Yahu^^ <phamgiahung02@gmail.com>
2023-03-27 18:26:52 +0700

    Add project files.

---
+++ UNITOFWORK/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UNITOFWORK.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ UNITOFWORK/Models/Order.cs
@@ -0,0 +1,6 @@
+﻿namespace UNITOFWORK.Models
+{
+    public class Order
+    {
+    }
+}
