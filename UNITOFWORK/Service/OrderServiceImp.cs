2004a53606dec0a4838c82427057648f0d3efcd5
Yahu^^ <phamgiahung02@gmail.com>
2023-03-27 19:15:27 +0700

    implment interface Product and Order

---
+++ UNITOFWORK/Service/OrderServiceImp.cs
@@ -0,0 +1,39 @@
+﻿using System.Security.AccessControl;
+using UNITOFWORK.Models;
+using UNITOFWORK.Repository;
+
+namespace UNITOFWORK.Service
+{
+    public class OrderServiceImp : IOrderRepository
+    {
+        private readonly DatabaseContext context;
+        public OrderServiceImp(DatabaseContext context)
+        {
+            this.context = context;
+        }
+        public void AddOrder(Order order)
+        {
+            context.Orders.Add(order);
+        }
+
+        public void DeleteOrder(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Order> GetOrders()
+        {
+            return context.Orders.ToList();
+        }
+
+        public Order OrderGetOrder(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateOrder(Order order)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UNITOFWORK/Service/ProductServiceImp.cs
@@ -0,0 +1,39 @@
+﻿using UNITOFWORK.Models;
+using UNITOFWORK.Repository;
+
+namespace UNITOFWORK.Service
+{
+    public class ProductServiceImp : IProductRepository
+    {
+        private readonly DatabaseContext context;
+        public ProductServiceImp(DatabaseContext context)
+        {
+            this.context = context;
+        }
+        public void AddProduct(Product product)
+        {
+            context.Products.Add(product);
+        }
+
+        public void DeleteProduct(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetOrders()
+        {
+            return context.Products.ToList();
+
+        }
+
+        public Product GetProduct(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
